d7c0b53a4e00f99014fe2c5056df0e3440ee38f1
Pedro Henrique <phfollador1@gmail.com>
2025-11-24 10:05:42 -0300

    Criando a class Transactions

---
+++ FinancialSystem/Dima/Dima.Core/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public DateTime? PaidOrReceivedAt { get; set; }
+        public int Type { get; set; }
+        public decimal Amount { get; set; }
+        public long CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
