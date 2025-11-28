e5f5c3772445431afc572d1cbce42cde314f2d6d
Pedro Henrique <phfollador1@gmail.com>
2025-11-28 10:20:32 -0300

    Entendendo a dinamica da api

--- FinancialSystem/Dima/Dima.Api/Program.cs
+++ FinancialSystem/Dima/Dima.Api/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Request
+// GET, POST, PUT & DELETE
+// GET (nao tem corpo)
+// REQUEST -> cabecalho e corpo
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
