786dcffb6919feefbb194796895b0adfcd4f4810
Pedro Henrique <phfollador1@gmail.com>
2025-12-01 10:40:15 -0300

    Responses

--- FinancialSystem/Dima/Dima.Api/Program.cs
+++ FinancialSystem/Dima/Dima.Api/Program.cs
@@ -7,11 +7,23 @@ var app = builder.Build();
 // REQUEST -> cabecalho e corpo
 
 // endpoints - url para acesso (https://localhos:0000/)
-app.MapGet("/", () => "Hello World!");
-app.MapPost("/", () => "Hello World!");
-app.MapPut("/", () => "Hello World!");
-app.MapDelete("/", () => "Hello World!");
 
 // versionamento: serao diversos fronts consumindo a api
 // mas pode haver alguma modificacao na api e determinada rota pode quebrar
+
+app.MapPost(
+    "/v1/transactions", 
+    () => new { message = "Hello" });
+
 app.Run();
+
+// Request
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreateAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
