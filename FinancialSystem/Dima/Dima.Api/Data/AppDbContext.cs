a3c9607b0627121f56e9a4589160feda2d979f84
Pedro Henrique <phfollador1@gmail.com>
2025-12-14 11:36:40 -0300

    Criando os DbSet's

--- FinancialSystem/Dima/Dima.Api/Data/AppDbContext.cs
+++ FinancialSystem/Dima/Dima.Api/Data/AppDbContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dima.Api.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Transaction> Transactions { get; set; } = null!;
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration();
+        }
     }
 }
