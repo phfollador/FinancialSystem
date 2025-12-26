5536922b8eead7de0836d77bd90a48b0304900be
Pedro Henrique <phfollador1@gmail.com>
2025-12-26 14:50:55 -0300

    Constructors and serialization

--- FinancialSystem/Dima/Dima.Core/Responses/Response.cs
+++ FinancialSystem/Dima/Dima.Core/Responses/Response.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Dima.Core.Responses
 {
-    public class Response
+    public class Response<T>
     {
         private readonly int code;
-        public string? Data { get; set; }
-        public string Message { get; set; } = string.Empty;
+        private const int defaultStatusCode = 200;
+        public T? Data { get; set; }
+        public string? Message { get; set; } = string.Empty;
         public bool IsSuccess => code is >= 200 and <= 299;
 
-        public Response(string data, string message, int code)
+        [JsonConstructor]
+        public Response()
+        {
+            code = defaultStatusCode;
+        }
+
+        public Response(T? data, int code = defaultStatusCode, string? message = null)
         {
             Data = data;
             Message = message;
