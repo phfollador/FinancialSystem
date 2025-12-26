bd4bc7654cc8c7a7f1ab65e49a49a59660c950f5
Pedro Henrique <phfollador1@gmail.com>
2025-12-26 15:04:24 -0300

    Pages configuration

--- FinancialSystem/Dima/Dima.Core/Responses/PagedResponse.cs
+++ FinancialSystem/Dima/Dima.Core/Responses/PagedResponse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Dima.Core.Responses
@@ -9,8 +10,22 @@ namespace Dima.Core.Responses
     public class PagedResponse<T> : Response<T>
     {
         public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
         public int PageSize { get; set; } = Configuration.DefaultPageSige;
         public int TotalCount { get; set; }
+
+        public PagedResponse(T? data, int code = Configuration.defaultStatusCode, string message = null) : base(data, code, message)
+        {
+            
+        }
+
+        [JsonConstructor]
+        public PagedResponse(T? data, int totalCount, int currentPage, int pageSize = Configuration.DefaultPageSige) : base(data) 
+        {
+            Data = data;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+        }
     }
 }
