f9b0c44eceaa9027073ef9b7f6dcf66950d00c1f
Pedro Henrique <phfollador1@gmail.com>
2025-12-26 09:54:57 -0300

    Creating responses pattern

---
+++ FinancialSystem/Dima/Dima.Core/Responses/Response.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Responses
+{
+    public class Response
+    {
+        private readonly int code;
+        public string? Data { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool IsSuccess => code is >= 200 and <= 299;
+
+        public Response(string data, string message, int code)
+        {
+            Data = data;
+            Message = message;
+            this.code = code;
+        }
+    }
+}
