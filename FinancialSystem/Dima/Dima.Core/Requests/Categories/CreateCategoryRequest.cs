3464c5184ce6a30d35a4d797b255bc6eaa070c76
Pedro Henrique <phfollador1@gmail.com>
2025-12-27 06:27:55 -0300

    Implementing CreateCategoryResponse.cs

--- FinancialSystem/Dima/Dima.Core/Requests/Categories/CreateCategoryRequest.cs
+++ FinancialSystem/Dima/Dima.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace Dima.Core.Requests.Categories
 {
     public class CreateCategoryRequest : Request
     {
+        [Required(ErrorMessage = "Invalid title")]
+        [MaxLength(80, ErrorMessage = "Max lenght is 80")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Invalid description")]
+        public string Description { get; set; } = string.Empty;
     }
 }
