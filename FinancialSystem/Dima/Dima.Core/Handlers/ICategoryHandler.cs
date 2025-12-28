2a52c4cdd2003e816b8e8e42c5c1090cebdc0dcb
Pedro Henrique <phfollador1@gmail.com>
2025-12-28 07:44:19 -0300

    Implementing interface

--- FinancialSystem/Dima/Dima.Core/Handlers/ICategoryHandler.cs
+++ FinancialSystem/Dima/Dima.Core/Handlers/ICategoryHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,10 @@ namespace Dima.Core.Handlers
 {
     public interface ICategoryHandler
     {
+        Task<Response<Category>> CreateAsync(CreateCategoryRequest request);
+        Task<Response<Category>> UpdateAsync(UpdateCategoryRequest request);
+        Task<Response<Category>> DeleteAsync(DeleteCategoryRequest request);
+        Task<Response<Category>> GetByIdAsync(GetCategoryByIdRequest request);
+        Task<Response<List<Category>>> GetAllAsync(GetAllCategoriesRequest request);
     }
 }
