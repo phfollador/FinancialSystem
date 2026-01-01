052652eb357fe6935de55b5cd9d55df344346674
Pedro Henrique <phfollador1@gmail.com>
2026-01-01 09:42:04 -0300

    Endpoints pattern

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -0,0 +1,12 @@
+﻿using Dima.Api.Data.Common.Api;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class CreateCategoryEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
