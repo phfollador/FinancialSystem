be0a9d9f945cfbd630e5d1dc07a3a8190e2cf37a
Pedro Henrique <phfollador1@gmail.com>
2026-01-03 09:57:08 -0300

    Creating MapEndpoints as extendio methon in Enpoint.cs

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Endpoint.cs
@@ -0,0 +1,11 @@
+﻿namespace Dima.Api.Endpoints
+{
+    public static class Endpoint
+    {
+        // extensio method
+        public static void MapEndpoints(this WebApplication app)
+        {
+
+        }
+    }
+}
