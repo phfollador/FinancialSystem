213a3cd8e714cc29f69f199c6751627a8f353a6d
Pedro Henrique <phfollador1@gmail.com>
2026-01-03 09:55:10 -0300

    Using 'With' to add swagger funcion information

--- FinancialSystem/Dima/Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -9,7 +9,12 @@ namespace Dima.Api.Endpoints.Categories
 {
     public class CreateCategoryEndpoint : IEndpoint
     {
-        public static void Map(IEndpointRouteBuilder app) => app.MapPost("", HandleAsync);
+        public static void Map(IEndpointRouteBuilder app) 
+            => app.MapPost("", HandleAsync)
+            .WithName("Categories: Create")
+            .WithSummary("Cria uma nova categoria")
+            .WithDescription("Cria uma nova categoria")
+            .WithOrder(1);
 
         public static async Task<IResult> HandleAsync(ICategoryHandler handler, CreateCategoryRequest request)
         {
