c15c01e0f53744608991fb2ce2f78feddc9aca4f
Pedro Henrique <phfollador1@gmail.com>
2026-01-04 10:28:14 -0300

    Creating GetAllCategories map segmented endpoint

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Categories/GetAllCategoriesEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class GetAllCategoriesEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("/", HandleAsync)
+            .WithName("Categories: Get All Categories")
+            .WithSummary("Obtem todas as categoria")
+            .WithDescription("Obtem todas as categoria")
+            .WithOrder(5)
+            .Produces<PagedResponse<List<Category>?>>();
+
+        private static async Task<IResult> HandleAsync(ICategoryHandler handler, [FromQuery]int pageNumber = Configuration.DefaultPageNumber, [FromQuery]int pageSize = Configuration.DefaultPageSige)
+        {
+            var request = new GetAllCategoriesRequest
+            {
+                UserId = "teste@pedro",
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+
+            var result = await handler.GetAllAsync(request);
+
+            if (result.IsSuccess)
+                return TypedResults.Ok(result?.Data);
+
+            return TypedResults.BadRequest();
+        }
+    }
+}
