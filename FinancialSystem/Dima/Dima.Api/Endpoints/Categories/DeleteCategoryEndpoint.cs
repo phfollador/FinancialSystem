297db70b004282e1964d571c78aeb86d7f0ab10c
Pedro Henrique <phfollador1@gmail.com>
2026-01-04 10:20:23 -0300

    Creating DeleteCategory map segmented endpoint

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Categories/DeleteCategoryEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class DeleteCategoryEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapDelete("{id}", HandleAsync)
+            .WithName("Categories: Delete")
+            .WithSummary("Remove uma categoria")
+            .WithDescription("Remove uma categoria")
+            .WithOrder(3)
+            .Produces<Response<Category?>>();
+
+        private static async Task<IResult> HandleAsync(ICategoryHandler handler, long id)
+        {
+            var request = new DeleteCategoryRequest
+            {
+                Id = id,
+                UserId = "teste@pedro"
+            };
+
+            var result = await handler.DeleteAsync(request);
+
+            if (result.IsSuccess)
+                return TypedResults.Ok(result?.Data);
+
+            return TypedResults.BadRequest();
+        }
+    }
+}
