6e5c95fd741097270395022407fb4249754e3064
Pedro Henrique <phfollador1@gmail.com>
2026-01-04 10:23:40 -0300

    Creating GetCategoryById map segmented endpoint

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Categories/GetCategoryByIdEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class GetCategoryByIdEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("{id}", HandleAsync)
+            .WithName("Categories: Get By Id")
+            .WithSummary("Obtem uma categoria")
+            .WithDescription("Obtem uma categoria")
+            .WithOrder(4)
+            .Produces<Response<Category?>>();
+
+        private static async Task<IResult> HandleAsync(ICategoryHandler handler, long id)
+        {
+            var request = new GetCategoryByIdRequest
+            {
+                Id = id,
+                UserId = "teste@pedro"
+            };
+
+            var result = await handler.GetByIdAsync(request);
+
+            if (result.IsSuccess)
+                return TypedResults.Ok(result?.Data);
+
+            return TypedResults.BadRequest();
+        }
+    }
+}
