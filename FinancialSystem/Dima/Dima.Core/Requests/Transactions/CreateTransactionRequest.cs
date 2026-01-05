8a2f9b6d17901e08e91210328300731f5ef292fa
Pedro Henrique <phfollador1@gmail.com>
2026-01-05 09:12:54 -0300

    Implementing CreateTransactionRequest

--- FinancialSystem/Dima/Dima.Core/Requests/Transactions/CreateTransactionRequest.cs
+++ FinancialSystem/Dima/Dima.Core/Requests/Transactions/CreateTransactionRequest.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dima.Core.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace Dima.Core.Requests.Transactions
 {
     public class CreateTransactionRequest
     {
+        [Required(ErrorMessage = "Invalid Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Invalid Type")]
+        public ETransactionType Type { get; set; }
+
+        [Required(ErrorMessage = "Invalid Value")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Invalid Category")]
+        public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Invalid Date")]
+        public DateTime? PaidOrReceivedAt { get; set; }
     }
 }
