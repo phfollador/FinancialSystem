d02d590050e3a62a1619bda018d192d2215433aa
Pedro Henrique <phfollador1@gmail.com>
2026-01-05 09:16:38 -0300

    Implementing UpdateTransactionRequest

---
+++ FinancialSystem/Dima/Dima.Core/Requests/Transactions/UpdateTransactionRequest.cs
@@ -0,0 +1,30 @@
+﻿using Dima.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Requests.Transactions
+{
+    public class UpdateTransactionRequest : Request
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Invalid Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Invalid Type")]
+        public ETransactionType Type { get; set; }
+
+        [Required(ErrorMessage = "Invalid Value")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Invalid Category")]
+        public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Invalid Date")]
+        public DateTime? PaidOrReceivedAt { get; set; }
+    }
+}
