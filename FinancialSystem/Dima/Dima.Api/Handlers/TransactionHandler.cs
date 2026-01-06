3a58c4d1f26a2d1fcf97a92398bf6f6b2baf6cf9
Pedro Henrique <phfollador1@gmail.com>
2026-01-06 08:42:40 -0300

    Implementing CreateAsync contract

--- FinancialSystem/Dima/Dima.Api/Handlers/TransactionHandler.cs
+++ FinancialSystem/Dima/Dima.Api/Handlers/TransactionHandler.cs
@@ -1,6 +1,57 @@
-﻿namespace Dima.Api.Handlers
+﻿using Dima.Api.Data;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Handlers
 {
-    public class TransactionHandler
+    public class TransactionHandler(AppDbContext context) : ITransactionHandler
     {
+        public async Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
+        {
+            try
+            {
+                var transaction = new Transaction
+                {
+                    UserId = request.UserId,
+                    CategoryId = request.CategoryId,
+                    CreateAt = DateTime.UtcNow,
+                    Amount = request.Amount,
+                    PaidOrReceivedAt = request.PaidOrReceivedAt,
+                    Title = request.Title,
+                    Type = request.Type
+                };
+
+                await context.Transactions.AddAsync(transaction);
+                await context.SaveChangesAsync();
+
+                return new Response<Transaction?>(transaction, 201, "Transacao criada com sucesso!");
+            }
+            catch
+            {
+                return new Response<Transaction?>(null, 500, "Nao foi possivel criar uma transacao");
+            }
+        }
+
+        public Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionsByPeriodRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
