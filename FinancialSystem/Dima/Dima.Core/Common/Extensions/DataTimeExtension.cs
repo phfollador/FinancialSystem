1347a0ae6e346f28709e1631d07dac955758a5e2
Pedro Henrique <phfollador1@gmail.com>
2026-01-07 20:28:32 -0300

    Create DateTime Extension

---
+++ FinancialSystem/Dima/Dima.Core/Common/Extensions/DataTimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Common.Extensions
+{
+    public static class DataTimeExtension
+    {
+        public static DateTime GetFirstDate(this DateTime date, int? year = null, int? month = null)
+        {
+            return new DateTime(year ?? date.Year, month ?? date.Month, 1);
+        }
+
+        public static DateTime GetLastDate(this DateTime date, int? year = null, int? month = null)
+        {
+            return new DateTime(year ?? date.Year, month ?? date.Month, 1).AddMonths(1).AddDays(-1);
+        }
+    }
+}
