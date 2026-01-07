a01b4a52188a2b4d6e242287e1a8242c66fdf05c
Pedro Henrique <phfollador1@gmail.com>
2026-01-07 20:30:42 -0300

    Code cleaning

--- FinancialSystem/Dima/Dima.Core/Requests/Transactions/DeleteTransactionRequest.cs
+++ FinancialSystem/Dima/Dima.Core/Requests/Transactions/DeleteTransactionRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dima.Core.Requests.Transactions
+﻿namespace Dima.Core.Requests.Transactions
 {
     public class DeleteTransactionRequest : Request
     {
--- FinancialSystem/Dima/Dima.Core/Requests/Transactions/GetTransactionByIdRequest.cs
+++ FinancialSystem/Dima/Dima.Core/Requests/Transactions/GetTransactionByIdRequest.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dima.Core.Requests.Transactions
+﻿namespace Dima.Core.Requests.Transactions
 {
     public class GetTransactionByIdRequest : Request
     {
