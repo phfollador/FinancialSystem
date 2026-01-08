f49a8edf3750ae8f238569ad73307430dc7ab931
Pedro Henrique <phfollador1@gmail.com>
2026-01-08 07:49:59 -0300

    Implementing GetTransactionById endpoint

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Transactions/GetTransactionByIdEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Transactions
+{
+    public class GetTransactionByIdEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("{id}", HandleAsync)
+            .WithName("Transactions: Get By Id")
+            .WithSummary("Obtem uma transaction")
+            .WithDescription("Obtem uma transaction")
+            .WithOrder(4)
+            .Produces<Response<Transaction?>>();
+
+        private static async Task<IResult> HandleAsync(ITransactionHandler handler, long id)
+        {
+            var request = new GetTransactionByIdRequest
+            {
+                Id = id,
+                UserId = "teste@pedro"
+            };
+
+            var result = await handler.GetByIdAsync(request);
+
+            if (result.IsSuccess)
+                return TypedResults.Ok(result);
+
+            return TypedResults.BadRequest(result);
+        }
+    }
+}
