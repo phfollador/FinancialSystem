e554c4ec3fd8abb883eeba65995b16e11cb99d1d
Pedro Henrique <phfollador1@gmail.com>
2026-01-08 07:42:28 -0300

    Implementing DeleteTransaction endpoint

--- FinancialSystem/Dima/Dima.Api/Endpoints/Transactions/DeleteTransactionEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Transactions/DeleteTransactionEndpoint.cs
@@ -1,6 +1,36 @@
-﻿namespace Dima.Api.Endpoints.Transactions
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+using System.Transactions;
+
+namespace Dima.Api.Endpoints.Transactions
 {
-    public class DeleteTransactionEndpoint
+    public class DeleteTransactionEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapPost("", HandleAsync)
+            .WithName("Transactions: Delete")
+            .WithSummary("Remove uma transaction")
+            .WithDescription("Remove uma transaction")
+            .WithOrder(2)
+            .Produces<Response<Transaction?>>();
+
+        private static async Task<IResult> HandleAsync(ITransactionHandler handler, long id)
+        {
+            var request = new DeleteTransactionRequest
+            {
+                Id = id,
+                UserId = "teste@pedro"
+            };
+
+            var result = await handler.DeleteAsync(request);
+
+            if (result.IsSuccess)
+                return TypedResults.Ok(result);
+
+            return TypedResults.BadRequest(result);
+        }
     }
 }
