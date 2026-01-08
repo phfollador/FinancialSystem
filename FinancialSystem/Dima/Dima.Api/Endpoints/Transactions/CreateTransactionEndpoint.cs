4d16c7c7aa61bfe79768c971d7a4ae7f26210707
Pedro Henrique <phfollador1@gmail.com>
2026-01-08 07:40:41 -0300

    Implemente CreateTransaction endpoint

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Transactions/CreateTransactionEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Transactions
+{
+    public class CreateTransactionEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapPost("", HandleAsync)
+            .WithName("Transactions: Create")
+            .WithSummary("Cria uma nova transaction")
+            .WithDescription("Cria uma nova transaction")
+            .WithOrder(1)
+            .Produces<Response<Transaction?>>();
+
+        private static async Task<IResult> HandleAsync(ITransactionHandler handler, CreateTransactionRequest request)
+        {
+            request.UserId = "teste@pedro";
+            var result = await handler.CreateAsync(request);
+
+            if (result.IsSuccess)
+                return TypedResults.Created($"/{result.Data?.Id}", result);
+
+            return TypedResults.BadRequest(result);
+        }
+    }
+}
---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Transactions/DeleteTransactionEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Dima.Api.Endpoints.Transactions
+{
+    public class DeleteTransactionEndpoint
+    {
+    }
+}
