69c509f08c46d87ad6599ae454685a260d0f0155
Pedro Henrique <phfollador1@gmail.com>
2026-01-08 07:46:32 -0300

    Implementing UpdateTransaction endpoint

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Transactions/UpdateTransactionEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Transactions
+{
+    public class UpdateTransactionEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapPut("{id}", HandleAsync)
+            .WithName("Transactions: Update")
+            .WithSummary("Atualiza uma transaction")
+            .WithDescription("Atualiza uma transaction")
+            .WithOrder(3)
+            .Produces<Response<Transaction?>>();
+
+        private static async Task<IResult> HandleAsync(ITransactionHandler handler, UpdateTransactionRequest request, long id)
+        {
+            request.Id = id;
+            request.UserId = "teste@pedro"; // fixed to tests
+
+            var result = await handler.UpdateAsync(request);
+
+            if (result.IsSuccess)
+                return TypedResults.Ok(result);
+
+            return TypedResults.BadRequest(result);
+        }
+    }
+}
