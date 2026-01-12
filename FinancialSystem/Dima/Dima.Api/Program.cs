0bb3409d170d6d36426260a90f005d895abffe7c
Pedro Henrique <phfollador1@gmail.com>
2026-01-12 19:08:53 -0300

    Setting Authentication methods

--- FinancialSystem/Dima/Dima.Api/Program.cs
+++ FinancialSystem/Dima/Dima.Api/Program.cs
@@ -5,6 +5,7 @@ using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Categories;
 using Dima.Core.Responses;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,7 +24,7 @@ builder.Services.AddSwaggerGen(x => x.CustomSchemaIds(n => n.FullName)); // gera
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
