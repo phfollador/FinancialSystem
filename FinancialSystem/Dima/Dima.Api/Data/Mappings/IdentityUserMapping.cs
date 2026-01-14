8d98d327dc1f02c2cfceff0a0435c6f468737e8a
Pedro Henrique <phfollador1@gmail.com>
2026-01-14 18:11:39 -0300

    Mapping IdentityUser

--- FinancialSystem/Dima/Dima.Api/Data/Mappings/IdentityUserMapping.cs
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/IdentityUserMapping.cs
@@ -1,6 +1,30 @@
-﻿namespace Dima.Api.Data.Mappings
+﻿using Dima.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
 {
-    public class IdentityUserMapping
+    public class IdentityUserMapping : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("IdentityUser");
+            builder.HasKey(t => t.Id);
+            builder.HasIndex(u => u.NormalizedUserName).IsUnique();
+            builder.HasIndex(u => u.NormalizedEmail).IsUnique();
+
+            builder.Property(u => u.Email).HasMaxLength(180);
+            builder.Property(u => u.NormalizedEmail).HasMaxLength(180);
+            builder.Property(u => u.UserName).HasMaxLength(180);
+            builder.Property(u => u.NormalizedUserName).HasMaxLength(90);
+            builder.Property(u => u.PhoneNumber).HasMaxLength(11);
+            builder.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+            builder.HasMany<IdentityUserClaim<long>>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+            builder.HasMany<IdentityUserLogin<long>>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+            builder.HasMany<IdentityUserToken<long>>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+            builder.HasMany<IdentityUserRole<long>>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+        }
     }
 }
