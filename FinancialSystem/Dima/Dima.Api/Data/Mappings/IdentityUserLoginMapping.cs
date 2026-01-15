d5cf46d58df477311aeff38a213535a6b40006d9
Pedro Henrique <phfollador1@gmail.com>
2026-01-15 17:22:47 -0300

    Creating IdentityUserTokenMapping class

--- FinancialSystem/Dima/Dima.Api/Data/Mappings/IdentityUserLoginMapping.cs
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/IdentityUserLoginMapping.cs
@@ -8,7 +8,7 @@ namespace Dima.Api.Data.Mappings
     {
         public void Configure(EntityTypeBuilder<IdentityUserLogin<long>> builder)
         {
-            builder.ToTable("IdentityClaim");
+            builder.ToTable("IdentityUserLogin");
             builder.HasKey(x => new { x.LoginProvider, x.ProviderKey });
             builder.Property(x => x.LoginProvider).HasMaxLength(128);
             builder.Property(x => x.ProviderKey).HasMaxLength(128);
---
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/IdentityUserTokenMapping.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class IdentityUserTokenMapping : IEntityTypeConfiguration<IdentityUserToken<long>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserToken<long>> builder)
+        {
+            builder.ToTable("IdentityUserToken");
+            builder.HasKey(x => new { x.UserId, x.LoginProvider, x.Name });
+            builder.Property(x => x.LoginProvider).HasMaxLength(128);
+            builder.Property(x => x.Name).HasMaxLength(180);
+        }
+    }
+}
