a963368908d066cf8ef008d4e955a82b1a2793e6
Pedro Henrique <phfollador1@gmail.com>
2026-01-15 17:27:19 -0300

    Creating IdentityRoleClaimMapping class

---
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/IdentityRoleClaimMapping.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class IdentityRoleClaimMapping : IEntityTypeConfiguration<IdentityRoleClaim<long>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRoleClaim<long>> builder)
+        {
+            builder.ToTable("IdentityRoleClaim");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.ClaimType).HasMaxLength(255);
+            builder.Property(x => x.ClaimValue).HasMaxLength(255);
+        }
+    }
+}
