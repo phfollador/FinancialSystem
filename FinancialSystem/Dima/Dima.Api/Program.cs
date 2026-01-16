ff758f9e7748098c03717048087e7d3b10a25c97
Pedro Henrique <phfollador1@gmail.com>
2026-01-16 17:48:56 -0300

    Adding Identity Core

--- FinancialSystem/Dima/Dima.Api/Program.cs
+++ FinancialSystem/Dima/Dima.Api/Program.cs
@@ -1,6 +1,7 @@
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Categories;
@@ -18,6 +19,8 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseSqlServer(connectionString);
 });
+
+builder.Services.AddIdentityCore<User>().AddRoles<IdentityRole<long>>().AddEntityFrameworkStores<AppDbContext>().AddApiEndpoints();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(x => x.CustomSchemaIds(n => n.FullName)); // gera o front para consultar a documentacao da api
