4b7ce60ffbd5ae3c9249431260b9b23d2f0ecf6f
Pedro Henrique <phfollador1@gmail.com>
2026-01-22 19:29:32 -0300

    Corrections on Program MapGet

--- FinancialSystem/Dima/Dima.Api/Program.cs
+++ FinancialSystem/Dima/Dima.Api/Program.cs
@@ -42,7 +42,7 @@ app.UseSwaggerUI();
 app.MapGet("/", () => new { message = "OK" });
 app.MapEndpoints();
 
-app.MapGroup("v1/identity").WithTags("Identity").MapPost("/logout", async (ClaimsPrincipal user) => 
+app.MapGroup("v1/identity").WithTags("Identity").MapGet("/roles", async (ClaimsPrincipal user) => 
 {
     if (user.Identity is null || !user.Identity.IsAuthenticated)
         return Results.Unauthorized();
