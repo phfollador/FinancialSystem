a78a8263cd0cd1201f2cbc73d94647caeae34e78
Pedro Henrique <phfollador1@gmail.com>
2026-01-29 09:51:44 -0300

    Creating BuilderExtensions to organize Program.cs

---
+++ FinancialSystem/Dima/Dima.Api/Data/Common/Api/BuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Dima.Api.Data.Common.Api
+{
+    public static class BuilderExtensions
+    {
+        public static void AddConfiguration(this WebApplicationBuilder webApplicationBuilder)
+        {
+
+        }
+    }
+}
