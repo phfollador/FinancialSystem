d8ee3b96b08b94bfe0b6571c9b78a8401f46bffd
Pedro Henrique <phfollador1@gmail.com>
2026-02-12 07:38:02 -0300

    Creating AccountHandler interface

---
+++ FinancialSystem/Dima/Dima.Core/Handlers/IAccountHandler.cs
@@ -0,0 +1,17 @@
+﻿using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Handlers
+{
+    public interface IAccountHandler
+    {
+        Task<Response<string>> LoginAsync(LoginRequest loginRequest);
+        Task<Response<string>> RegisterAsync(RegisterRequest loginRequest);
+        Task LogoutAsync();
+    }
+}
