22ac421226eeff2591f0b95c66293e81263773b3
Pedro Henrique <phfollador1@gmail.com>
2026-02-14 07:01:29 -0300

    Using HttpClientFactory to construct AccountHandler

---
+++ FinancialSystem/Dima/Dima.Web/Handlers/AccountHandler.cs
@@ -0,0 +1,27 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+using System.Net.Http.Json;
+
+namespace Dima.Web.Handlers
+{
+    public class AccountHandler(IHttpClientFactory httpClientFactory) : IAccountHandler
+    {
+        private readonly HttpClient client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
+        public async Task<Response<string>> LoginAsync(LoginRequest loginRequest)
+        {
+            await client.PostAsJsonAsync("v1/identity/login", loginRequest);
+        }
+
+        public Task LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<string>> RegisterAsync(RegisterRequest loginRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
