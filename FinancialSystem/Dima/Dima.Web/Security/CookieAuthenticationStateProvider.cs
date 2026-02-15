957e5436b5e9683f61f5476e38486fd7e963b8bd
Pedro Henrique <phfollador1@gmail.com>
2026-02-15 08:18:00 -0300

    Working with users and claims

--- FinancialSystem/Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
+++ FinancialSystem/Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Dima.Core.Models.Account;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+using System.Security.Claims;
 
 namespace Dima.Web.Security
 {
@@ -21,6 +24,29 @@ namespace Dima.Web.Security
         public void NotifyAuthenticationStateChanged()
         {
             base.NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        private async Task<User?> GetUser()
+        {
+            try
+            {
+                return await client.GetFromJsonAsync<User?>("v1/identity/manage/info");
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private async Task<List<Claim>> GetClaims(User user)
+        {
+            var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim(ClaimTypes.Email, user.Email)
+                };
+
+            return claims;
         }
     }
 }
