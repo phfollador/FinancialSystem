8ab86f8fa27e96d964a6a4c9659471bf80f571ee
Pedro Henrique <phfollador1@gmail.com>
2026-02-15 08:02:21 -0300

    ImplementinggICookieAuthenticationStateProvider

--- FinancialSystem/Dima/Dima.Web/Security/ICookieAuthenticationStateProvider.cs
+++ FinancialSystem/Dima/Dima.Web/Security/ICookieAuthenticationStateProvider.cs
@@ -1,7 +1,19 @@
-﻿namespace Dima.Web.Security
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Dima.Web.Security
 {
     public interface ICookieAuthenticationStateProvider
     {
+        Task<bool> CheckAuthenticationAsync();
+        Task<AuthenticationState> GetAuthenticationStateAsync();
+        void NotifyAuthenticationStateChanged();
+    }
 
+    public class CookieAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
