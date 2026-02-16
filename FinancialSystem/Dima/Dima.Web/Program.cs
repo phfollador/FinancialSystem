bad9a0bb369de992a9462ff7f5d536cc63c476da
Pedro Henrique <phfollador1@gmail.com>
2026-02-16 16:29:25 -0300

    Creating Cookie Handler

--- FinancialSystem/Dima/Dima.Web/Program.cs
+++ FinancialSystem/Dima/Dima.Web/Program.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Dima.Web;
 using MudBlazor.Services;
+using Dima.Web.Security;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<CookieHandler>();
 
 builder.Services.AddMudServices();
 
