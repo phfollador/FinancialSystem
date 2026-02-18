65bb973c64a66f31b98d48ac93449a5416583b68
Pedro Henrique <phfollador1@gmail.com>
2026-02-18 10:16:43 -0300

    Resolving dependencies

--- FinancialSystem/Dima/Dima.Web/Program.cs
+++ FinancialSystem/Dima/Dima.Web/Program.cs
@@ -3,12 +3,21 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Dima.Web;
 using MudBlazor.Services;
 using Dima.Web.Security;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<CookieHandler>();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddScoped(x => (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
 
 builder.Services.AddMudServices();
 
