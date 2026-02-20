d4fb6a6608b139eb8bbf44ccd708e07e6cd5398c
Pedro Henrique <phfollador1@gmail.com>
2026-02-20 10:33:20 -0300

    Injection

--- FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Web.Security;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Identity
 {
     public partial class RegisterPage : ComponentBase
     {
+        public MudForm MudForm {  get; set; }
 
+        [Inject]
+        public CookieAuthenticationStateProvider AuthState { get; set; } = null!;
     }
 }
