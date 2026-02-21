e0e2013563c37a63166095037c177350ae10e107
Pedro Henrique <phfollador1@gmail.com>
2026-02-21 10:47:17 -0300

    Inserting add transient to account handler in Program.cs

--- FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
@@ -6,9 +6,6 @@ namespace Dima.Web.Pages.Identity
 {
     public partial class RegisterPage : ComponentBase
     {
-        public MudForm MudForm {  get; set; }
 
-        [Inject]
-        public CookieAuthenticationStateProvider AuthState { get; set; } = null!;
     }
 }
