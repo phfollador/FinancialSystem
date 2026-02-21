12e58b8a082c7c2f0d1730245c158f368f4cf119
Pedro Henrique <phfollador1@gmail.com>
2026-02-21 10:51:15 -0300

    Resolving Register dependencies

--- FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,11 +1,26 @@
-﻿using Dima.Web.Security;
+﻿using Dima.Core.Handlers;
+using Dima.Web.Security;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
+using System.ComponentModel;
 
 namespace Dima.Web.Pages.Identity
 {
     public partial class RegisterPage : ComponentBase
     {
+        #region Dependencies
+        [Inject]
+        public IAccountHandler Handler { get; set; } = null!;
 
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+        #endregion
     }
 }
