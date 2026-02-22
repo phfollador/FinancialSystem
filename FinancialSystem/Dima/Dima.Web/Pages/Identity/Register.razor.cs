d1fd720bfe03abe7e9c12dc5037445394f6eb716
Pedro Henrique <phfollador1@gmail.com>
2026-02-22 08:04:18 -0300

    Register code behind

--- FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
 using Dima.Web.Security;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -10,6 +11,7 @@ namespace Dima.Web.Pages.Identity
     public partial class RegisterPage : ComponentBase
     {
         #region Dependencies
+
         [Inject]
         public IAccountHandler Handler { get; set; } = null!;
 
@@ -21,6 +23,52 @@ namespace Dima.Web.Pages.Identity
 
         [Inject]
         public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsBusy { get; set; } = false;
+        public RegisterRequest InputModel { get; set; } = new();
+
+        #endregion
+
+        #region Overrides
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity is { IsAuthenticated: true}) // se o usuario e diferente de null e esta autenticado (forma diferente e elegante)
+                NavigationManager.NavigateTo("/");
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task OnValidSubmitAsync()
+        {
+            try
+            {
+                var result = await Handler.RegisterAsync(InputModel);
+
+                if (result.IsSuccess)
+                    NavigationManager.NavigateTo("/login");
+                else
+                    Snackbar.Add(result.Message, Severity.Error);
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
         #endregion
     }
 }
