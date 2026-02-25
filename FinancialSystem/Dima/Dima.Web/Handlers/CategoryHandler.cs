0e9b4c1aec7d3ae06e3f2b4ea27b3b2784a268b4
Pedro Henrique <phfollador1@gmail.com>
2026-02-24 21:36:29 -0300

    Creating CategoryHandler to consume Dima.Api

---
+++ FinancialSystem/Dima/Dima.Web/Handlers/CategoryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers
+{
+    public class CategoryHandler : ICategoryHandler
+    {
+        public Task<Response<Category?>> CreateAsync(CreateCategoryRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategoriesRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
