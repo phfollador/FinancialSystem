a63e41b12a68a05dfbc4b17a37e61c8b71dec291
Pedro Henrique <phfollador1@gmail.com>
2026-02-24 21:35:46 -0300

    Creating TransactionHandler to consume Dima.Api

--- FinancialSystem/Dima/Dima.Web/Configuration.cs
+++ FinancialSystem/Dima/Dima.Web/Configuration.cs
@@ -26,9 +26,9 @@ namespace Dima.Web
                 AppbarBackground = new MudBlazor.Utilities.MudColor("#1EFA2D"),
                 AppbarText = Colors.Shades.Black,
                 TextPrimary = Colors.Shades.Black,
-                PrimaryContrastText = Colors.Shades.Black,
-                DrawerText = Colors.Shades.Black,
-                DrawerBackground = Colors.LightGreen.Lighten4
+                PrimaryContrastText = new MudBlazor.Utilities.MudColor("#000000"),
+                DrawerText = Colors.Shades.White,
+                DrawerBackground = Colors.Green.Darken4
             },
 
             PaletteDark = new PaletteDark()
@@ -36,7 +36,8 @@ namespace Dima.Web
                 Primary = Colors.LightGreen.Accent3,
                 Secondary = Colors.LightGreen.Darken3,
                 AppbarBackground = Colors.LightGreen.Accent3,
-                AppbarText = Colors.Shades.Black
+                AppbarText = Colors.Shades.Black,
+                PrimaryContrastText = new MudBlazor.Utilities.MudColor("#000000")
             },
         };
     }
