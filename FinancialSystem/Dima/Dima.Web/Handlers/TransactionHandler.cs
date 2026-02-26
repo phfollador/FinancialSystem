64ad8b2c862efded16c5183024990565e246273e
Pedro Henrique <phfollador1@gmail.com>
2026-02-26 11:20:16 -0300

    Get transaction by period on transactions handler

--- FinancialSystem/Dima/Dima.Web/Handlers/TransactionHandler.cs
+++ FinancialSystem/Dima/Dima.Web/Handlers/TransactionHandler.cs
@@ -1,12 +1,16 @@
-﻿using Dima.Core.Handlers;
+﻿using Dima.Core.Common.Extensions;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Transactions;
 using Dima.Core.Responses;
+using System.Net.Http.Json;
 
 namespace Dima.Web.Handlers
 {
-    public class TransactionHandler : ITransactionHandler
+    public class TransactionHandler(IHttpClientFactory httpClientFactory) : ITransactionHandler
     {
+        private readonly HttpClient client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
         public Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
         {
             throw new NotImplementedException();
@@ -22,9 +26,22 @@ namespace Dima.Web.Handlers
             throw new NotImplementedException();
         }
 
-        public Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionsByPeriodRequest request)
+        public async Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionsByPeriodRequest request)
         {
-            throw new NotImplementedException();
+            const string format = "yyyy-MM-dd";
+
+            var startDate = request.StartDate is not null 
+                ? request.StartDate.Value.ToString(format) 
+                : DateTime.Now.GetFirstDate().ToString(format);
+
+            var endDate = request.EndDate is not null 
+                ? request.EndDate.Value.ToString(format)
+                : DateTime.Now.GetLastDate().ToString(format);
+
+            var url = $"v1/transactions?startDate={startDate}&endDate={endDate}";
+
+            return await client.GetFromJsonAsync<PagedResponse<List<Transaction>?>>(url) 
+                ?? new PagedResponse<List<Transaction>?>(null, 400, "Nao foi possivel obter as transacoes");
         }
 
         public Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
