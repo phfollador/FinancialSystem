cd940359aa2671560569317d38b5d3c782e5c548
Pedro Henrique <phfollador1@gmail.com>
2026-03-02 08:43:55 -0300

    Implementing Create.razor.cs

--- FinancialSystem/Dima/Dima.Web/Pages/Categories/Create.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Categories/Create.razor.cs
@@ -1,8 +1,56 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Requests.Categories;
+using Dima.Web.Handlers;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Categories
 {
     public class CreateCategoryPage : ComponentBase
     {
+        #region Properties
+
+        public bool IsBusy { get; set; } = false;
+        public CreateCategoryRequest InputModel { get; set; } = new();
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public CategoryHandler Handler { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        #endregion
+
+        #region Methods
+
+        public async Task OnValidSubmitAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var result = await Handler.CreateAsync(InputModel);
+                if (result.IsSuccess)
+                    NavigationManager.NavigateTo("/categorias");
+                else
+                    Snackbar.Add(result.Message, Severity.Error);
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
     }
 }
