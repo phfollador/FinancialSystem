1406bd5d1f245fcaf3694defc3aa7db8edaac7fd
Pedro Henrique <phfollador1@gmail.com>
2026-03-18 08:02:48 -0300

    Implementing Properties, Parameters and Services in EditCategoryPage

--- FinancialSystem/Dima/Dima.Web/Pages/Categories/Edit.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Categories/Edit.razor.cs
@@ -1,8 +1,31 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Components;
 
 namespace Dima.Web.Pages.Categories
 {
     public partial class EditCategoryPage : ComponentBase
     {
+        #region Properties
+
+        public bool IsBusy { get; set; } = false;
+
+        #endregion
+
+        #region Parameters
+
+        [Parameter]
+        public string Id { get; set; } = string.Empty;
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject]
+        public ICategoryHandler Handler { get; set; } = null!;
+
+        #endregion
     }
 }
