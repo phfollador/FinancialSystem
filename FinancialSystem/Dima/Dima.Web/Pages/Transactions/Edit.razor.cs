87fad4ce4cce2192c1e941bdd6c97d28b2cb4525
Pedro Henrique <phfollador1@gmail.com>
2026-03-31 07:45:17 -0300

    Implementing Edit razor page and EditTransactionPage partial class

--- FinancialSystem/Dima/Dima.Web/Pages/Transactions/Edit.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Transactions/Edit.razor.cs
@@ -1,8 +1,139 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Transactions
 {
     public partial class EditTransactionPage : ComponentBase
     {
+        #region Properties
+
+        [Parameter]
+        public string Id { get; set; } = string.Empty;
+        public bool IsBusy { get; set; } = false;
+        public UpdateTransactionRequest InputModel { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public ICategoryHandler CategoryHandler { get; set; } = null!;
+
+        [Inject]
+        public ITransactionHandler TransactionHandler { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        #endregion
+
+        #region Overrides
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsBusy = true;
+
+            await GetTransactionByIdAsync();
+            await GetCategoriesAsync();
+
+            IsBusy = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task OnValidSubmitAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var result = await TransactionHandler.UpdateAsync(InputModel);
+                if (result.IsSuccess)
+                    NavigationManager.NavigateTo("/lancamentos/historico");
+                else
+                    Snackbar.Add(result.Message, Severity.Error);
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private async Task GetTransactionByIdAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var request = new GetTransactionByIdRequest { Id = long.Parse(Id)};
+                var result = await TransactionHandler.GetByIdAsync(request);
+
+                if (result.IsSuccess && result.Data is not null)
+                {
+                    InputModel = new UpdateTransactionRequest
+                    {
+                        CategoryId = result.Data.CategoryId,
+                        PaidOrReceivedAt = result.Data.PaidOrReceivedAt,
+                        Title = result.Data.Title,
+                        Amount = result.Data.Amount,
+                        Id = result.Data.Id,
+                        Type = result.Data.Type
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task GetCategoriesAsync()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var request = new GetAllCategoriesRequest();
+                var result = await CategoryHandler.GetAllAsync(request);
+
+                if (result.IsSuccess)
+                {
+                    Categories = result.Data ?? [];
+                    InputModel.CategoryId = Categories.FirstOrDefault()?.Id ?? 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
     }
 }
