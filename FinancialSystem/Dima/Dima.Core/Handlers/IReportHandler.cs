7b7de9cf09e66df71add49b9a4744cf48ca1a662
Pedro Henrique <phfollador1@gmail.com>
2026-04-05 10:17:48 -0300

    Implementing IReportHandler interface

---
+++ FinancialSystem/Dima/Dima.Core/Handlers/IReportHandler.cs
@@ -0,0 +1,19 @@
+﻿using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Dima.Core.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Handlers
+{
+    public interface IReportHandler
+    {
+        Task<Response<FinancialSummary?>> GetFinancialSummaryReportAsync(GetFinancialSummaryRequest request);
+        Task<Response<List<IncomesAndExpenses>?>> GetIncomesAndExpensesReportAsync(GetIncomesAndExpensesRequest request);
+        Task<Response<List<IncomesByCategory>?>> GetIncomesByCategoryReportAsync(GetIncomesByCategoryRequest request);
+        Task<Response<List<ExpensesByCategory>?>> GetExpensesByCategoryReportAsync(GetExpensesByCategoryRequest request);
+    }
+}
