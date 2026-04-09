1ef6ff7a0f058e02412d5a0300715d04f50cf8ab
Pedro Henrique <phfollador1@gmail.com>
2026-04-09 17:21:03 -0300

    Implementing GetIncomesByCategoryEndpoint

--- FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetIncomesByCategoryEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetIncomesByCategoryEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace Dima.Api.Endpoints.Reports
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Dima.Core.Responses;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Reports
 {
-    public class GetIncomesByCategoryEndpoint
+    public class GetIncomesByCategoryEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("/expenses", HandleAsync)
+            .Produces<Response<List<IncomesByCategory>?>>();
+
+        private static async Task<IResult> HandleAsync(ClaimsPrincipal user, GetIncomesByCategoryRequest request, IReportHandler handler)
+        {
+            request.UserId = user.Identity?.Name ?? string.Empty;
+            var result = await handler.GetIncomesByCategoryReportAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
+        }
     }
 }
