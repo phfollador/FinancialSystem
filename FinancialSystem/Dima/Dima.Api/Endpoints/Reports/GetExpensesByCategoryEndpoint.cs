156578502d4dc1b2fdc27ca91e78a467bc369fe5
Pedro Henrique <phfollador1@gmail.com>
2026-04-09 17:17:00 -0300

    Implementing GetExpensesByCategoryEndpoint

--- FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetExpensesByCategoryEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetExpensesByCategoryEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace Dima.Api.Endpoints.Reports
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Dima.Core.Responses;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Reports
 {
-    public class GetExpensesByCategoryEndpoint
+    public class GetExpensesByCategoryEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app) 
+            => app.MapGet("/expenses", HandleAsync)
+            .Produces<Response<List<ExpensesByCategory>?>>();
+
+        private static async Task<IResult> HandleAsync(ClaimsPrincipal user, GetExpensesByCategoryRequest request, IReportHandler handler)
+        {
+            request.UserId = user.Identity?.Name ?? string.Empty;
+            var result = await handler.GetExpensesByCategoryReportAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
+        }
     }
 }
