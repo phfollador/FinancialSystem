99cab08ba0eac40310a24e59c3f93c4df0d4ec02
Pedro Henrique <phfollador1@gmail.com>
2026-04-09 17:19:33 -0300

    Implementing GetFinancialSummaryEndpoint

--- FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetFinancialSummaryEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetFinancialSummaryEndpoint.cs
@@ -1,6 +1,23 @@
-﻿namespace Dima.Api.Endpoints.Reports
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Dima.Core.Responses;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Reports
 {
-    public class GetFinancialSummaryEndpoint
+    public class GetFinancialSummaryEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("/summary", HandleAsync)
+            .Produces<Response<FinancialSummary?>>();
+
+        private static async Task<IResult> HandleAsync(ClaimsPrincipal user, GetFinancialSummaryRequest request, IReportHandler handler)
+        {
+            request.UserId = user.Identity?.Name ?? string.Empty;
+            var result = await handler.GetFinancialSummaryReportAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
+        }
     }
 }
