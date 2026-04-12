887d81db195d3f41484e185bf0477c41c9d4f1e0
Pedro Henrique <phfollador1@gmail.com>
2026-04-12 17:28:49 -0300

    Correcting request creation on GetIncomesByCategory

--- FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetIncomesByCategoryEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Reports/GetIncomesByCategoryEndpoint.cs
@@ -13,9 +13,13 @@ namespace Dima.Api.Endpoints.Reports
             => app.MapGet("/incomes", HandleAsync)
             .Produces<Response<List<IncomesByCategory>?>>();
 
-        private static async Task<IResult> HandleAsync(ClaimsPrincipal user, GetIncomesByCategoryRequest request, IReportHandler handler)
+        private static async Task<IResult> HandleAsync(ClaimsPrincipal user, IReportHandler handler)
         {
-            request.UserId = user.Identity?.Name ?? string.Empty;
+            var request = new GetIncomesByCategoryRequest
+            {
+                UserId = user.Identity?.Name ?? string.Empty
+            };
+
             var result = await handler.GetIncomesByCategoryReportAsync(request);
             return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
         }
