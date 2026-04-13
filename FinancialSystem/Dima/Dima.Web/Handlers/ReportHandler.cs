0f9eb501d0a234073b8923abb76537e9925b08f2
Pedro Henrique <phfollador1@gmail.com>
2026-04-13 17:42:17 -0300

    Creating ReportHandler class and app debug

---
+++ FinancialSystem/Dima/Dima.Web/Handlers/ReportHandler.cs
@@ -0,0 +1,30 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers
+{
+    public class ReportHandler : IReportHandler
+    {
+        public Task<Response<List<ExpensesByCategory>?>> GetExpensesByCategoryReportAsync(GetExpensesByCategoryRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<FinancialSummary?>> GetFinancialSummaryReportAsync(GetFinancialSummaryRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<IncomesAndExpenses>?>> GetIncomesAndExpensesReportAsync(GetIncomesAndExpensesRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<IncomesByCategory>?>> GetIncomesByCategoryReportAsync(GetIncomesByCategoryRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
