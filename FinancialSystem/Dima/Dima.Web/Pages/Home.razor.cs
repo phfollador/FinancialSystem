379aead269e3159cd4c52b67fe101d578cfe320b
Pedro Henrique <phfollador1@gmail.com>
2026-04-18 08:45:44 -0300

    Implementing Home.razor.cs

--- FinancialSystem/Dima/Dima.Web/Pages/Home.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Home.razor.cs
@@ -1,8 +1,46 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages
 {
     public partial class HomePage : ComponentBase
     {
+        #region Properties
+
+        public bool ShowValues { get; set; } = true;
+        public FinancialSummary FinancialSummary { get; set; }
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public IReportHandler Handler { get; set; } = null!;
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        #endregion
+
+        #region Overrides
+
+        protected override async Task OnInitializedAsync()
+        {
+            var request = new GetFinancialSummaryRequest();
+            var result = await Handler.GetFinancialSummaryReportAsync(request);
+
+            if (result.IsSuccess && result.Data is not null)
+                FinancialSummary = result.Data;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        public void ToggleShowValues() => ShowValues = !ShowValues;
+
+        #endregion
     }
 }
