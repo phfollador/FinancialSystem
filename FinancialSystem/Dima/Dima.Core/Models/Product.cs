f6a98208953b55a103bc0fb55cf15d2af040798e
Pedro Henrique <phfollador1@gmail.com>
2026-04-20 10:29:18 -0300

    Implementing Voucher class

--- FinancialSystem/Dima/Dima.Core/Models/Product.cs
+++ FinancialSystem/Dima/Dima.Core/Models/Product.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dima.Core.Models.Reports
+namespace Dima.Core.Models
 {
     internal class Product
     {
--- FinancialSystem/Dima/Dima.Core/Models/Voucher.cs
+++ FinancialSystem/Dima/Dima.Core/Models/Voucher.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Dima.Core.Models
 {
-    internal class Voucher
+    public class Voucher
     {
+        public long Id { get; set; }
+        public string Number { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public decimal Amount { get; set; }
     }
 }
