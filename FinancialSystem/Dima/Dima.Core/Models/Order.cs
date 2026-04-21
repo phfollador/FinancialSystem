55647f0d9fa668d3ff307c7c328d945a7052f52c
Pedro Henrique <phfollador1@gmail.com>
2026-04-21 08:56:05 -0300

    Implementing Order fields

--- FinancialSystem/Dima/Dima.Core/Models/Order.cs
+++ FinancialSystem/Dima/Dima.Core/Models/Order.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Dima.Core.Models
 {
-    internal class Order
+    public class Order
     {
+        public long Id { get; set; }
+        public string Number { get; set; } = Guid.NewGuid().ToString("N")[..8]; // pega os 8 primeiros caracteres
+        public string UserId { get; set; } = string.Empty;
+        public long ProductId { get; set; }
+        public Product Product { get; set; } = null!;
+        public long VoucherId { get; set; }
+        public Voucher Voucher { get; set; } = null!;
+
+        public decimal Total => Product.Price - (Voucher?.Amount ?? 0);
     }
 }
