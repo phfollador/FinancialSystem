cbe6533f2a07a600936c184ce1dbf3a21d1bcce7
Pedro Henrique <phfollador1@gmail.com>
2026-04-21 08:44:15 -0300

    Implementing Voucher fields

--- FinancialSystem/Dima/Dima.Core/Models/Product.cs
+++ FinancialSystem/Dima/Dima.Core/Models/Product.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Dima.Core.Models
 {
-    internal class Product
+    public class Product
     {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Desctiption { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public decimal Price { get; set; }
     }
 }
--- FinancialSystem/Dima/Dima.Core/Models/Voucher.cs
+++ FinancialSystem/Dima/Dima.Core/Models/Voucher.cs
@@ -15,5 +15,7 @@ namespace Dima.Core.Models
         public string Notes { get; set; } = string.Empty;
         public bool IsActive { get; set; }
         public decimal Amount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
