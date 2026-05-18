a91311e55d8dba929eea5625f4d9a5bc1e37bb3e
Pedro Henrique <phfollador1@gmail.com>
2026-05-18 10:31:15 -0300

    Implementing OrderHandler interface

--- FinancialSystem/Dima/Dima.Core/Handlers/IOrderHandler.cs
+++ FinancialSystem/Dima/Dima.Core/Handlers/IOrderHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Dima.Core.Models;
+using Dima.Core.Requests;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,11 @@ namespace Dima.Core.Handlers
 {
     internal interface IOrderHandler
     {
+        Task<Response<Order?>> CancelAsync(CancelOrderRequest request);
+        Task<Response<Order?>> CreateAsync(CreateOrderRequest request);
+        Task<Response<Order?>> PayAsync(PayOrderRequest request);
+        Task<Response<Order?>> RefoundAsync(RefoundOrderRequest request);
+        Task<PagedResponse<List<Order>?>> GetAllAsync(GetAllOrdersRequest request);
+        Task<PagedResponse<Order?>> GetByNumberAsync(GetOrderByNumberRequest request);
     }
 }
