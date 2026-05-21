c6699d611043779aacce122086fe924fe6e92530
Pedro Henrique <phfollador1@gmail.com>
2026-05-21 10:35:23 -0300

    Creating VoucherHandler interface

---
+++ FinancialSystem/Dima/Dima.Core/Handlers/IVoucherHandler.cs
@@ -0,0 +1,16 @@
+﻿using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Handlers
+{
+    internal interface IVoucherHandler
+    {
+        Task<Response<Voucher?>> GetByNumberAsync(GetVoucherByNumberRequest request);
+    }
+}
