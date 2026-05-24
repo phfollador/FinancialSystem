33804eecbe4f62b1006d293987ca93a32c5aa8eb
Pedro Henrique <phfollador1@gmail.com>
2026-05-24 10:49:33 -0300

    Implementing ProductMaping

--- FinancialSystem/Dima/Dima.Api/Data/Mappings/ProductMapping.cs
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/ProductMapping.cs
@@ -4,11 +4,35 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Dima.Api.Data.Mappings
 {
-    public class ProductMapping : IEntityTypeConfiguration<Voucher>
+    public class ProductMapping : IEntityTypeConfiguration<Product>
     {
-        public void Configure(EntityTypeBuilder<Voucher> builder)
+        public void Configure(EntityTypeBuilder<Product> builder)
         {
-            
+            builder.ToTable("Product");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired(true)
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Desctiption)
+                .IsRequired(false)
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Slug)
+                .IsRequired(true)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Price)
+                .IsRequired(true)
+                .HasColumnType("MONEY");
+
+            builder.Property(x => x.IsActive)
+                .IsRequired(true)
+                .HasColumnType("BIT");
         }
     }
 }
