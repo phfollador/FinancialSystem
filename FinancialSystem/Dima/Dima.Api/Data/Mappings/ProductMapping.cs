722762c21f36a25409a8f993fd36ea1ece36625b
Pedro Henrique <phfollador1@gmail.com>
2026-05-24 10:45:28 -0300

    Creating ProductMapping

---
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/ProductMapping.cs
@@ -0,0 +1,14 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class ProductMapping : IEntityTypeConfiguration<Voucher>
+    {
+        public void Configure(EntityTypeBuilder<Voucher> builder)
+        {
+            
+        }
+    }
+}
