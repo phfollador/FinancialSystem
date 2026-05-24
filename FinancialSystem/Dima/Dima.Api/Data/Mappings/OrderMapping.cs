6e0d190537d9b76243bd51ba097d231e35f4e743
Pedro Henrique <phfollador1@gmail.com>
2026-05-24 10:50:39 -0300

    Creating OrderMapping

---
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/OrderMapping.cs
@@ -0,0 +1,14 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class OrderMapping : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
