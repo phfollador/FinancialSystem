2ff63709d0dbbd023bf9e20077fed0f3c5e8e99b
Pedro Henrique <phfollador1@gmail.com>
2026-05-24 10:55:53 -0300

    Implementing OrderMapping

--- FinancialSystem/Dima/Dima.Api/Data/Mappings/OrderMapping.cs
+++ FinancialSystem/Dima/Dima.Api/Data/Mappings/OrderMapping.cs
@@ -8,7 +8,45 @@ namespace Dima.Api.Data.Mappings
     {
         public void Configure(EntityTypeBuilder<Order> builder)
         {
-            throw new NotImplementedException();
+            builder.ToTable("Order");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Number)
+                .IsRequired(true)
+                .HasColumnType("CHAR")
+                .HasMaxLength(8);
+
+            builder.Property(x => x.ExternalReference)
+                .IsRequired(false)
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(60);
+
+            builder.Property(x => x.Gateway)
+                .IsRequired(true)
+                .HasColumnType("SMALLINT");
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired(true)
+                .HasColumnType("DATETIME2");
+
+            builder.Property(x => x.UpdatedAt)
+                .IsRequired(true)
+                .HasColumnType("DATETIME2");
+
+            builder.Property(x => x.Status)
+                .IsRequired(true)
+                .HasColumnType("SMALLINT");
+
+            builder.Property(x => x.UserId)
+                .IsRequired(false)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(160);
+
+            builder.HasOne(x => x.Product)
+                .WithMany();
+
+            builder.HasOne(x => x.Voucher)
+                .WithMany();
         }
     }
 }
