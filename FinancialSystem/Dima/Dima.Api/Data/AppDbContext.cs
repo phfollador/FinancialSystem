f60b538b153834dca7410dacef4dc437c13610ef
Pedro Henrique <phfollador1@gmail.com>
2026-05-25 07:47:30 -0300

    Updating AppDbContext file to insert Product, Voucher and Order do generate a new migration

--- FinancialSystem/Dima/Dima.Api/Data/AppDbContext.cs
+++ FinancialSystem/Dima/Dima.Api/Data/AppDbContext.cs
@@ -22,6 +22,9 @@ namespace Dima.Api.Data
     {
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<Transaction> Transactions { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Voucher> Vouchers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
         public DbSet<IncomesAndExpenses> IncomesAndExpenses { get; set; } = null!;
         public DbSet<IncomesByCategory> IncomesByCategories { get; set; } = null!;
         public DbSet<ExpensesByCategory> ExpensesByCategories { get; set; } = null!;
