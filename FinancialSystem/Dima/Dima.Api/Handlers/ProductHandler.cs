0581818c5236b940456d0c1431957276d97e8ac4
Pedro Henrique <phfollador1@gmail.com>
2026-05-27 07:36:22 -0300

    Implementing inherits on ProductHandler

--- FinancialSystem/Dima/Dima.Api/Handlers/ProductHandler.cs
+++ FinancialSystem/Dima/Dima.Api/Handlers/ProductHandler.cs
@@ -1,6 +1,20 @@
-﻿namespace Dima.Api.Handlers
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Handlers
 {
-    public class ProductHandler
+    public class ProductHandler : IProductHandler
     {
+        public Task<PagedResponse<List<Product>?>> GetAllAsync(GetAllProductsRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Product?>> GetBySlugAsync(GetProductBySlugRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
