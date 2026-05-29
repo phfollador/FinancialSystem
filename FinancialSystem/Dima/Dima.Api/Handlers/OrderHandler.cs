c4c4c5c16a6e67891f5abed0f503b88df5debf1e
Pedro Henrique <phfollador1@gmail.com>
2026-05-29 07:47:44 -0300

    Starting with CancelOrder implementation

--- FinancialSystem/Dima/Dima.Api/Handlers/OrderHandler.cs
+++ FinancialSystem/Dima/Dima.Api/Handlers/OrderHandler.cs
@@ -1,6 +1,57 @@
-﻿namespace Dima.Api.Handlers
+﻿using Dima.Api.Data;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dima.Api.Handlers
 {
-    public class OrderHandler
+    public class OrderHandler(AppDbContext context) : IOrderHandler
     {
+        public async Task<Response<Order?>> CancelAsync(CancelOrderRequest request)
+        {
+            Order? order;
+
+            try
+            {
+                order = await context.Orders
+                    .Include(x => x.Product)
+                    .Include(x => x.Voucher)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+                if (order == null)
+                    return new Response<Order?>(null, 404, "Pedido nao encontrado");
+            }
+            catch
+            {
+                return new Response<Order?>(null, 500, "Falha ao obter pedido");
+            }
+        }
+
+        public Task<Response<Order?>> CreateAsync(CreateOrderRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Order>?>> GetAllAsync(GetAllOrdersRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<Order?>> GetByNumberAsync(GetOrderByNumberRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Order?>> PayAsync(PayOrderRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Order?>> RefoundAsync(RefoundOrderRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
