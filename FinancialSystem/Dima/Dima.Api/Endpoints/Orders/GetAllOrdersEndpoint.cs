680c98e61c43434154dc54fedd50a3e223b576e3
Pedro Henrique <phfollador1@gmail.com>
2026-06-08 09:00:55 -0300

    Implementing GetAllOrdersEndpoint

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetAllOrdersEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetAllOrdersEndpoint.cs
@@ -1,6 +1,37 @@
-﻿namespace Dima.Api.Endpoints.Orders
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Orders
 {
-    public class GetAllOrdersEndpoint
+    public class GetAllOrdersEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app) 
+            => app.MapGet("/", HandleAsync)
+            .WithName("Orders: Get All")
+            .WithSummary("Recupera todos os pedidos")
+            .WithDescription("Recupera todos os pedidos")
+            .WithOrder(5)
+            .Produces<PagedResponse<List<Order>?>>();
+
+        private static async Task<IResult> HandleAsync(ClaimsPrincipal user, IOrderHandler handler, 
+            [FromQuery] int pageNumber = Configuration.DefaultPageNumber, 
+            [FromQuery] int pageSize = Configuration.DefaultPageSige)
+        {
+            var request = new GetAllOrdersRequest
+            {
+                UserId = user.Identity!.Name ?? string.Empty,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+
+            var result = await handler.GetAllAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
+        }
     }
 }
