2a78f9b10e90e0c0a88506b780652273e12d8e51
Pedro Henrique <phfollador1@gmail.com>
2026-06-07 21:25:25 -0300

    Implementing CreateOrderEndpoint

---
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/CreateOrderEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Orders
+{
+    public class CreateOrderEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app) 
+            => app.MapPost("/", HandleAsync)
+            .WithName("Orders: Creates a new order")
+            .WithSummary("Cria um novo pedido")
+            .WithDescription("Cria um novo pedido")
+            .Produces<Response<Order?>>();
+
+        private static async Task<IResult> HandleAsync(IOrderHandler handler, CreateOrderRequest request, ClaimsPrincipal user)
+        {
+            var result = await handler.CreateAsync(request);
+            return result.IsSuccess ? TypedResults.Created($"v1/orders/{result.Data?.Number}") : TypedResults.BadRequest(result);
+        }
+    }
+}
