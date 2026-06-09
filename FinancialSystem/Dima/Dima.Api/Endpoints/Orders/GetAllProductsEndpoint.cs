b32e0e95e2a219a2998345254c18be9261040e29
Pedro Henrique <phfollador1@gmail.com>
2026-06-09 08:08:01 -0300

    Implementing IEndpoint interface

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetAllProductsEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetAllProductsEndpoint.cs
@@ -1,6 +1,12 @@
-﻿namespace Dima.Api.Endpoints.Orders
+﻿using Dima.Api.Data.Common.Api;
+
+namespace Dima.Api.Endpoints.Orders
 {
-    public class GetAllProductsEndpoint
+    public class GetAllProductsEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
