0bd67adfc67b843105689e5c552e09ce68beb62c
Pedro Henrique <phfollador1@gmail.com>
2026-06-09 08:11:01 -0300

    Implementing HandleAsync method

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetAllProductsEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetAllProductsEndpoint.cs
@@ -1,4 +1,8 @@
 ﻿using Dima.Api.Data.Common.Api;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Orders;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dima.Api.Endpoints.Orders
 {
@@ -7,6 +11,18 @@ namespace Dima.Api.Endpoints.Orders
         public static void Map(IEndpointRouteBuilder app)
         {
             throw new NotImplementedException();
+        }
+
+        private static async Task<IResult> HandleAsync(IProductHandler handler, [FromQuery] int pageNumber = Configuration.DefaultPageNumber, [FromQuery] int pageSize = Configuration.DefaultPageSige)
+        {
+            var request = new GetAllProductsRequest
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+            };
+
+            var result = await handler.GetAllAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(request) : TypedResults.BadRequest(request);
         }
     }
 }
