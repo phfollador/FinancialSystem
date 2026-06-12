b807c701bb693caaafb59fcdbd04505a12001337
Pedro Henrique <phfollador1@gmail.com>
2026-06-12 09:07:26 -0300

    Calling IEndpoint interface

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetProductByIdEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetProductByIdEndpoint.cs
@@ -1,6 +1,8 @@
-﻿namespace Dima.Api.Endpoints.Orders
+﻿using Dima.Api.Data.Common.Api;
+
+namespace Dima.Api.Endpoints.Orders
 {
-    public class GetProductByIdEndpoint
+    public class GetProductByIdEndpoint : IEndpoint
     {
     }
 }
