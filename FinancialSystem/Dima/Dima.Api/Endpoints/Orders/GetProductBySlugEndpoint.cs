430ed25b55a7f0f9265c9e0a2c468cec1be32d58
Pedro Henrique <phfollador1@gmail.com>
2026-06-15 09:10:55 -0300

    Implementing HandleAsync method

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetProductBySlugEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetProductBySlugEndpoint.cs
@@ -1,5 +1,6 @@
 ﻿using Dima.Api.Data.Common.Api;
 using Dima.Core.Handlers;
+using Dima.Core.Requests.Orders;
 
 namespace Dima.Api.Endpoints.Orders
 {
@@ -12,7 +13,13 @@ namespace Dima.Api.Endpoints.Orders
 
         private static async Task<IResult> HandleAsync(IProductHandler handler, string slug)
         {
+            var request = new GetProductBySlugRequest
+            {
+                Slug = slug
+            };
 
+            var result = await handler.GetBySlugAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
         }
     }
 }
