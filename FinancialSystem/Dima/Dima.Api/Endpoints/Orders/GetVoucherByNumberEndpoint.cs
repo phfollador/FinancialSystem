64a57594518ec0fa976d4ad407312b02e57ef687
Pedro Henrique <phfollador1@gmail.com>
2026-06-18 09:17:21 -0300

    Calling IEnpoint interface

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetVoucherByNumberEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetVoucherByNumberEndpoint.cs
@@ -1,6 +1,8 @@
-﻿namespace Dima.Api.Endpoints.Orders
+﻿using Dima.Api.Data.Common.Api;
+
+namespace Dima.Api.Endpoints.Orders
 {
-    public class GetVoucherByNumberEndpoint
+    public class GetVoucherByNumberEndpoint : IEndpoint
     {
     }
 }
