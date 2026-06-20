044ac48d79dc597ffd56d252ed4bd436cfe0f3c0
Pedro Henrique <phfollador1@gmail.com>
2026-06-20 09:20:13 -0300

    Implementing HandleAsync method

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetVoucherByNumberEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetVoucherByNumberEndpoint.cs
@@ -1,8 +1,25 @@
 ﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Orders;
 
 namespace Dima.Api.Endpoints.Orders
 {
     public class GetVoucherByNumberEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task<IResult> HandleAsync(IVoucherHandler handler, string number)
+        {
+            var request = new GetVoucherByNumberRequest
+            {
+                Number = number
+            };
+
+            var result = await handler.GetByNumberAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
+        }
     }
 }
