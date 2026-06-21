e5a7467963857f72a999a92360313b9f071de19e
Pedro Henrique <phfollador1@gmail.com>
2026-06-21 09:33:56 -0300

    Implementing Map method

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/PayOrderEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/PayOrderEndpoint.cs
@@ -1,16 +1,21 @@
 ﻿using Dima.Api.Data.Common.Api;
 using Dima.Core.Handlers;
+using Dima.Core.Models;
 using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
 using System.Security.Claims;
 
 namespace Dima.Api.Endpoints.Orders
 {
     public class PayOrderEndpoint : IEndpoint
     {
-        public static void Map(IEndpointRouteBuilder app)
-        {
-            throw new NotImplementedException();
-        }
+        public static void Map(IEndpointRouteBuilder app) 
+            => app.MapPost("/{id}/pay", HandleAsync)
+            .WithName("Orders: Pay an order")
+            .WithSummary("Paga um pedido")
+            .WithDescription("Paga um pedido")
+            .WithOrder(3)
+            .Produces<Response<Order?>>();
 
         private static async Task<IResult> HandleAsync(IOrderHandler handler, long id, PayOrderRequest request, ClaimsPrincipal user)
         {
