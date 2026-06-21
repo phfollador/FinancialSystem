06e90dc65401676a44e14697ed2fc2f862d944df
Pedro Henrique <phfollador1@gmail.com>
2026-06-21 09:31:43 -0300

    Implementing HandleAsync method

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/PayOrderEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/PayOrderEndpoint.cs
@@ -1,4 +1,7 @@
 ﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Orders;
+using System.Security.Claims;
 
 namespace Dima.Api.Endpoints.Orders
 {
@@ -7,6 +10,15 @@ namespace Dima.Api.Endpoints.Orders
         public static void Map(IEndpointRouteBuilder app)
         {
             throw new NotImplementedException();
+        }
+
+        private static async Task<IResult> HandleAsync(IOrderHandler handler, long id, PayOrderRequest request, ClaimsPrincipal user)
+        {
+            request.Id = id;
+            request.UserId = user.Identity!.Name ?? string.Empty;
+
+            var result = await handler.PayAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
         }
     }
 }
