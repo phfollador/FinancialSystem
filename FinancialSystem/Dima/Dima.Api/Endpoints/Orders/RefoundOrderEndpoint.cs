644b59f608ddabdb8eb5266f5cc76bef70739b98
Pedro Henrique <phfollador1@gmail.com>
2026-06-22 10:09:52 -0300

    Calling interface

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/RefoundOrderEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/RefoundOrderEndpoint.cs
@@ -1,6 +1,12 @@
-﻿namespace Dima.Api.Endpoints.Orders
+﻿using Dima.Api.Data.Common.Api;
+
+namespace Dima.Api.Endpoints.Orders
 {
-    public class RefoundOrderEndpoint
+    public class RefoundOrderEndpoint : IEndpoint
     {
+        public static void Map(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
