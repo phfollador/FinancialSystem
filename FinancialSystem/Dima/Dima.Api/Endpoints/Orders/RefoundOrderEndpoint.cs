fc3781562514c581b8bc59f930779708751523b8
Pedro Henrique <phfollador1@gmail.com>
2026-06-22 10:16:11 -0300

    Implementing Map method

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/RefoundOrderEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/RefoundOrderEndpoint.cs
@@ -1,16 +1,20 @@
 ﻿using Dima.Api.Data.Common.Api;
 using Dima.Core.Handlers;
+using Dima.Core.Models;
 using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
 using System.Security.Claims;
 
 namespace Dima.Api.Endpoints.Orders
 {
     public class RefoundOrderEndpoint : IEndpoint
     {
-        public static void Map(IEndpointRouteBuilder app)
-        {
-            throw new NotImplementedException();
-        }
+        public static void Map(IEndpointRouteBuilder app) 
+            => app.MapPost("/{id}/refound", HandleAsync)
+            .WithName("Orders: refound order")
+            .WithSummary("Estorna um pedido")
+            .WithDescription("Estorna um pedido")
+            .WithOrder(6).Produces<Response<Order?>>();
 
         private static async Task<IResult> HandleAsync(IOrderHandler handler, long id, ClaimsPrincipal user)
         {
