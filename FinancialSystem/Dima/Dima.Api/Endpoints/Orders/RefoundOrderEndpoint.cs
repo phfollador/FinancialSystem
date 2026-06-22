5a9536b8f087244d44028141dcba28afd35b8b87
Pedro Henrique <phfollador1@gmail.com>
2026-06-22 10:13:34 -0300

    Implementing HandleAsync method

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/RefoundOrderEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/RefoundOrderEndpoint.cs
@@ -1,4 +1,7 @@
 ﻿using Dima.Api.Data.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Orders;
+using System.Security.Claims;
 
 namespace Dima.Api.Endpoints.Orders
 {
@@ -7,6 +10,18 @@ namespace Dima.Api.Endpoints.Orders
         public static void Map(IEndpointRouteBuilder app)
         {
             throw new NotImplementedException();
+        }
+
+        private static async Task<IResult> HandleAsync(IOrderHandler handler, long id, ClaimsPrincipal user)
+        {
+            var request = new RefoundOrderRequest
+            {
+                Id = id,
+                UserId = user.Identity!.Name ?? string.Empty
+            };
+
+            var result = await handler.RefoundAsync(request);
+            return result.IsSuccess ? TypedResults.Ok(result) : TypedResults.BadRequest(result);
         }
     }
 }
