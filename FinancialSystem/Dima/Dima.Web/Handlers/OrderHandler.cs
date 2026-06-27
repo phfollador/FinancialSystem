8bd7f88cbd50b44f84b7ce094a177289ba2da25f
Pedro Henrique <phfollador1@gmail.com>
2026-06-27 09:14:49 -0300

    [Frontend] Implementing interfaces to Handlers

--- FinancialSystem/Dima/Dima.Web/Handlers/OrderHandler.cs
+++ FinancialSystem/Dima/Dima.Web/Handlers/OrderHandler.cs
@@ -1,6 +1,40 @@
-﻿namespace Dima.Web.Handlers
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers
 {
-    public class OrderHandler
+    public class OrderHandler : IOrderHandler
     {
+        public Task<Response<Order?>> CancelAsync(CancelOrderRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Order?>> CreateAsync(CreateOrderRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Order>?>> GetAllAsync(GetAllOrdersRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<Order?>> GetByNumberAsync(GetOrderByNumberRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Order?>> PayAsync(PayOrderRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Order?>> RefoundAsync(RefoundOrderRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- FinancialSystem/Dima/Dima.Web/Handlers/VoucherHandler.cs
+++ FinancialSystem/Dima/Dima.Web/Handlers/VoucherHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Dima.Web.Handlers
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers
 {
-    public class VoucherHandler
+    public class VoucherHandler : IVoucherHandler
     {
+        public Task<Response<Voucher?>> GetByNumberAsync(GetVoucherByNumberRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
