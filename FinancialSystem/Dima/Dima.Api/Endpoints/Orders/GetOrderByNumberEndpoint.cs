ee730fc384e17dba5707bce516d6f55d22d906a5
Pedro Henrique <phfollador1@gmail.com>
2026-06-26 21:26:42 -0300

    Changing map type on Map method of GetOrderByNumberEndpoint

--- FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetOrderByNumberEndpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Orders/GetOrderByNumberEndpoint.cs
@@ -10,7 +10,7 @@ namespace Dima.Api.Endpoints.Orders
     public class GetOrderByNumberEndpoint : IEndpoint
     {
         public static void Map(IEndpointRouteBuilder app) 
-            => app.MapPost("/{number}", HandleAsync)
+            => app.MapGet("/{number}", HandleAsync)
             .WithName("Orders: Get By Number")
             .WithSummary("Get order by number")
             .WithDescription("Get order by number")
