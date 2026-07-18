311348c881106be6b380cd9370fd8f9b5df73b08
Pedro Henrique <phfollador1@gmail.com>
2026-07-17 21:54:59 -0300

    Starting List page component base implementation

--- FinancialSystem/Dima/Dima.Web/Pages/Products/List.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Products/List.razor.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Products
 {
     public class ListProductsPage : ComponentBase
     {
+        #region Properties
+
+        public List<Product> Products { get; set; } = [];
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public IProductHandler Handler { get; set; } = null!;
+
+        #endregion
+
     }
 }
