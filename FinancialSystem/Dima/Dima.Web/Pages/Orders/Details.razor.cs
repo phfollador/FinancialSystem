3d6f6cd49a098e5ffa17b00c5734ea2bb4a6164b
Pedro Henrique <phfollador1@gmail.com>
2026-08-12 23:13:13 -0300

    DetailsPage initial implementation

--- FinancialSystem/Dima/Dima.Web/Pages/Orders/Details.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Orders/Details.razor.cs
@@ -1,8 +1,53 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Orders
 {
     public class DetailsPage : ComponentBase
     {
+        #region Parameters
+
+        [Parameter]
+        public string Number { get; set; } = string.Empty;
+
+        #endregion
+
+        #region Properties
+
+        public Order Order { get; set; } = null!;
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public IOrderHandler Handler { get; set; } = null!;
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        #endregion
+
+        #region Overrides
+
+        protected override async Task OnInitializedAsync()
+        {
+            var request = new GetOrderByNumberRequest
+            {
+                Number = Number
+            };
+
+            var result = await Handler.GetByNumberAsync(request);
+
+            if (result.IsSuccess)
+                Order = result.Data;
+            else
+                Snackbar.Add(result.Message, Severity.Error);
+        }
+
+        #endregion
     }
 }
