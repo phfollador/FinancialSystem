8f7b7190ee95e5f08adb46592797ac3741762a9a
Pedro Henrique <phfollador1@gmail.com>
2026-08-18 20:57:36 -0300

    Calling OrderStatus on Details page

--- FinancialSystem/Dima/Dima.Web/Components/Orders/OrderStatus.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Components/Orders/OrderStatus.razor.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Enums;
+using Microsoft.AspNetCore.Components;
 
 namespace Dima.Web.Components.Orders
 {
     public partial class OrderStatusComponent : ComponentBase
     {
+        #region Parameters
+
+        [Parameter]
+        [EditorRequired]
+        public EOrderStatus Status { get; set; }
+
+        #endregion
     }
 }
