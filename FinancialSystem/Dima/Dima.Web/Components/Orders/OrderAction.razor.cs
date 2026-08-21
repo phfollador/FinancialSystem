4a0343316de88c40ebf3347df91c5094b42579d9
Pedro Henrique <phfollador1@gmail.com>
2026-08-20 21:08:21 -0300

    Inserting switch structure

--- FinancialSystem/Dima/Dima.Web/Components/Orders/OrderAction.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Components/Orders/OrderAction.razor.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace Dima.Web.Components.Orders
 {
     public partial class OrderActionComponent : ComponentBase
     {
+        #region Parameters
+
+        [Parameter]
+        [EditorRequired]
+        public Order Order { get; set; } = null!;
+
+        #endregion
     }
 }
