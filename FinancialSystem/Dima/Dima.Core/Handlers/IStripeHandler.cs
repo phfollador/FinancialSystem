0d972c5465cacdc43c6ef6807e4b0e8a7d9d71d9
Pedro Henrique <phfollador1@gmail.com>
2026-08-23 07:58:01 -0300

    Creating Session handler

---
+++ FinancialSystem/Dima/Dima.Core/Handlers/IStripeHandler.cs
@@ -0,0 +1,14 @@
+﻿using Dima.Core.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Handlers
+{
+    internal interface IStripeHandler
+    {
+        Task<Response<string?>> CreateSessionAsync();
+    }
+}
