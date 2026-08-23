fd8b89f32a48320c0dc86d400b07eebb0e8a8912
Pedro Henrique <phfollador1@gmail.com>
2026-08-23 08:05:35 -0300

    Implementing StripeTransactionsResponse

--- FinancialSystem/Dima/Dima.Core/Responses/Stripe/StripeTransactionsResponse.cs
+++ FinancialSystem/Dima/Dima.Core/Responses/Stripe/StripeTransactionsResponse.cs
@@ -2,5 +2,12 @@
 {
     public class StripeTransactionsResponse
     {
+        public string Id { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public long Amount { get; set; }
+        public long AmountCaptured { get; set; }
+        public bool Paid { get; set; }
+        public bool Refounded { get; set; }
     }
 }
