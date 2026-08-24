3ab4519a31d918b2780a171f2421e1ccb7e89a19
Pedro Henrique <phfollador1@gmail.com>
2026-08-24 07:20:58 -0300

    Creating StripeHandler

---
+++ FinancialSystem/Dima/Dima.Api/Handlers/StripeHandler.cs
@@ -0,0 +1,20 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Stripe;
+using Dima.Core.Responses;
+using Dima.Core.Responses.Stripe;
+
+namespace Dima.Api.Handlers
+{
+    public class StripeHandler : IStripeHandler
+    {
+        public async Task<Response<string?>> CreateSessionAsync(CreateSessionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<StripeTransactionsResponse>>> GetTransactionsByOrderNumberAsync(GetTransactionsByOrderNumberRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
