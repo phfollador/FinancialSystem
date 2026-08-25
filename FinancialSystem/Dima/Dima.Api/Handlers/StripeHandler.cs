e18cd837666bfd5a944820bb2babb72a928ba5b1
Pedro Henrique <phfollador1@gmail.com>
2026-08-25 07:31:24 -0300

    Implementing StripeHandler CreateSessionAsync method

--- FinancialSystem/Dima/Dima.Api/Handlers/StripeHandler.cs
+++ FinancialSystem/Dima/Dima.Api/Handlers/StripeHandler.cs
@@ -1,7 +1,10 @@
-﻿using Dima.Core.Handlers;
+﻿using Dima.Core;
+using Dima.Core.Handlers;
 using Dima.Core.Requests.Stripe;
 using Dima.Core.Responses;
 using Dima.Core.Responses.Stripe;
+using Stripe;
+using Stripe.Checkout;
 
 namespace Dima.Api.Handlers
 {
@@ -9,7 +12,43 @@ namespace Dima.Api.Handlers
     {
         public async Task<Response<string?>> CreateSessionAsync(CreateSessionRequest request)
         {
-            throw new NotImplementedException();
+            var options = new SessionCreateOptions 
+            {
+                CustomerEmail = request.UserId,
+                PaymentIntentData = new SessionPaymentIntentDataOptions
+                {
+                    Metadata = new Dictionary<string, string>
+                    {
+                        { "order", request.OrderNumber }
+                    }
+                },
+                PaymentMethodTypes = ["card", "boleto", "pix"],
+                LineItems = 
+                [
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            Currency = "BRL",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = request.ProductTitle,
+                                Description = request.ProductDescription
+                            },
+                            UnitAmount = request.OrderTotal
+                        },
+                        Quantity = 1
+                    }
+                ],
+                Mode = "payment",
+                SuccessUrl = $"{Configuration.FrontendUrl}/pedidos/{request.OrderNumber}/confirmar",
+                CancelUrl = $"{Configuration.FrontendUrl}/pedidos/{request.OrderNumber}/cancelar"
+            };
+
+            var service = new SessionService();
+            var session = await service.CreateAsync(options);
+
+            return new Response<string?>(session.Id);
         }
 
         public Task<Response<List<StripeTransactionsResponse>>> GetTransactionsByOrderNumberAsync(GetTransactionsByOrderNumberRequest request)
