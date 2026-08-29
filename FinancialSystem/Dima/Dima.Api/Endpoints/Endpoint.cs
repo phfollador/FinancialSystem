73fe5167fef2e3f8570f3727cc185d52f8affc4a
Pedro Henrique <phfollador1@gmail.com>
2026-08-29 07:51:48 -0300

    Mapping Stripe endpoint group

--- FinancialSystem/Dima/Dima.Api/Endpoints/Endpoint.cs
+++ FinancialSystem/Dima/Dima.Api/Endpoints/Endpoint.cs
@@ -3,6 +3,7 @@ using Dima.Api.Endpoints.Categories;
 using Dima.Api.Endpoints.Identity;
 using Dima.Api.Endpoints.Orders;
 using Dima.Api.Endpoints.Reports;
+using Dima.Api.Endpoints.Stripe;
 using Dima.Api.Endpoints.Transactions;
 using Dima.Api.Models;
 
@@ -55,6 +56,11 @@ namespace Dima.Api.Endpoints
                 .MapEndpoint<CancelOrderEndpoint>()
                 .MapEndpoint<PayOrderEndpoint>()
                 .MapEndpoint<RefoundOrderEndpoint>();
+
+            endpoints.MapGroup("v1/payments/stripe")
+                .WithTags("Payments - Stripe")
+                .RequireAuthorization()
+                .MapEndpoint<CreateSessionEndpoint>();
 
             endpoints.MapGroup("v1/identity")
                 .WithTags("Identity")
