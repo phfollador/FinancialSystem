f30e4e9ddc4f085a6c7169905dfe3c5698fbec41
Pedro Henrique <phfollador1@gmail.com>
2026-09-16 08:33:13 -0300

    Implementing refions

--- FinancialSystem/Dima/Dima.Web/Pages/Orders/Confirm.razor.cs
+++ FinancialSystem/Dima/Dima.Web/Pages/Orders/Confirm.razor.cs
@@ -1,8 +1,42 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Orders
 {
     public partial class ConfirmOrderPaymentPage : ComponentBase
     {
+        #region Parameters
+
+        [Parameter]
+        public string Number { get; set; } = string.Empty;
+
+        #endregion
+
+        #region Properties
+
+        public Order? Order { get; set; }
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public IOrderHandler OrderHandler { get; set; } = null!;
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        #endregion
+
+        #region Overrides
+
+        protected override async Task OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        #endregion
     }
 }
